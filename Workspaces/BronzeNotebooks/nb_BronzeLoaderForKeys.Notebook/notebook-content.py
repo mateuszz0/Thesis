ed363e82828a3abbd9fc134e29c2f56cb51d1998
sgh123156 <134965082+sgh123156@users.noreply.github.com>
2024-05-14 19:29:17 +0200

    Update notebook-content.py

--- Workspaces/BronzeNotebooks/nb_BronzeLoaderForKeys.Notebook/notebook-content.py
+++ Workspaces/BronzeNotebooks/nb_BronzeLoaderForKeys.Notebook/notebook-content.py
@@ -12,12 +12,13 @@ FileFormat = "Format in which a data files are ingested"
 from pyspark.sql.functions import sha2, concat_ws
 
 # CELL ********************
-
+# Load the Bronze-Landing data into Spark DataFrame 
 df = spark.read.format(FileFormat) \
         .load(f'abfss://Lakehouse@onelake.dfs.fabric.microsoft.com/Bronze.Lakehouse/Files/{SourceSystem}/{SourceSchema}/{LoadMode}/{SourceTable}/keys/{SourceTable}.parquet')
 
 df = df.withColumn('HashKey', sha2(concat_ws('|', 'Source', *[key.strip() for key in SourceKey.split(',')]), 256))
 
+# Write into Bronze-Raw (only hashkey) to later used for soft deletions
 df.select('HashKey').write.mode('overwrite') \
     .format('delta') \
     .option("overwriteSchema", 'true') \
